d7527b3abc0c5afcf85cb51167ccf00bf247e816
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-11-16 16:22:50 +0100

    Build the first Logic verify if in and if equal to 1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -17,11 +18,31 @@ void printArray(int arr[][SIZE],int SIZE){
 
 struct Cell{
   int row,col;
+  vector<struct Cell> visitedList;
+
   Cell(int i , int j){
     row = i;
     col = j;
   }
+
+  Cell(int i, int j,vector<struct Cell> visitedIn){
+    row = i;
+    col = j;
+    visitedList = visitedIn;
+    visitedList.push_back(Cell(row,col));
+  }
+
 };
+
+void visit(queue<Cell> q, Cell cell ,int array[][SIZE]){
+  //Verfie that cell inside the grid
+  if((cell.row < SIZE && cell.row >= 0 ) && (cell.col < SIZE-1 && cell.col >= 0)){
+    //verify if the value is equal to 1
+    if(array[cell.row][cell.col] == 1){
+      q.push(cell);
+    }
+  }
+}
 
 int main(){
   //Draw the map of cities
@@ -36,6 +57,15 @@ int main(){
   };
 
   printArray(array,SIZE);
+
+  //Search BFS
+
+  queue<Cell> q;
+
+  Cell start(0,0);
+  Cell target(SIZE,SIZE-1);
+
+  visit(q, start , array);
 
 
   return 0;
